506922943d3af0d030c9390aa43cd23b1f0db542
AnaliseWalker <analisemermaid@gmail.com>
2026-03-04 15:30:20 +0000

    finished walker project

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -11,8 +11,14 @@ function runProgram(){
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
   
-  // Game Item Objects
-
+  // Game Item Objects 
+  //caps mean keyboard and non caps mean arrows
+  const key = {enter: 13, LEFT: 65, left: 37, UP: 87, up: 38, RIGHT: 68, right: 39, down: 83, DOWN: 40};
+  var walker = {x: 0, y: 0, speedX: 0, speedY: 0};
+  var sidesWasPressed = false;
+  var heightWasPressed = false;
+  var boardWidth =  $("#board").width() - $("#walker").width();
+  var boardHeight = $("#board").height() - $("#walker").height();
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
@@ -23,7 +29,8 @@ function runProgram(){
 
   Note: You can have multiple event listeners for different types of events.
   */
-  $(document).on('eventType', handleEvent);                          
+  $(document).on('keydown', handleKeyDown);   
+  $(document).on('keyup', handleKeyUp);                      
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -34,7 +41,10 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
+    repositionGameItem();
+    // console.log(walker.speedX, walker.speedY);
+    wallCollision();
+    redrawGameItem();
 
   }
   
@@ -44,15 +54,82 @@ function runProgram(){
   
   Note: You can have multiple event handlers for different types of events.
   */
-  function handleEvent(event) {
+  function handleKeyDown(event) {
+    // console.log(event.which);
+    if (event.which === key.left || event.which === key.LEFT){
+      // console.log("left pressed");
+      walker.speedX = -5;
+      sidesWasPressed = true;
+    }
+    else if (event.which === key.right || event.which === key.RIGHT){
+      // console.log("right pressed");
+      walker.speedX = 5;
+      sidesWasPressed = true;
+    }
+    else if (event.which === key.down || event.which === key.DOWN){
+      // console.log("down pressed");
+      walker.speedY = 5;
+      heightWasPressed = true;
+    }
+    else if (event.which === key.enter){
+      // console.log("enter pressed");
+      var randomColor = "#000000".replace(/0/g, function () {
+      return (~~(Math.random() * 16)).toString(16);
+      });
+      $("#walker").css("background-color", randomColor);
+    }
+    else if (event.which === key.up || event.which === key.UP){
+      // console.log("up pressed");
+      walker.speedY = -5;
+      heightWasPressed = true;
+    }
+  }
 
+  function handleKeyUp(event){
+    if (sidesWasPressed === true){
+      sidesWasPressed = false;
+      walker.speedX = 0;
+    }
+    else if(heightWasPressed === true){
+      heightWasPressed = false;
+      walker.speedY = 0;
+    }
   }
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
 
-  
+  function repositionGameItem(){
+    walker.x += walker.speedX;
+    walker.y += walker.speedY;
+  }
+
+  function redrawGameItem(){
+    $("#walker").css("left", walker.x);
+    $("#walker").css("top", walker.y);
+    // console.log("Walker position:", walker.x, walker.y);
+  }
+
+  function wallCollision(){
+    if (walker.x < 0){
+      walker.x += 5;
+    }
+    else if (walker.x > boardWidth){
+      walker.x -= 5;
+    }
+    else if (walker.y < 0){
+      walker.y += 5;
+    }
+    else if (walker.y > boardHeight){
+      walker.y -= 5;
+    }
+  }
+
+  // function spawnEnemies() {
+
+  // }
+
   function endGame() {
     // stop the interval timer
     clearInterval(interval);
