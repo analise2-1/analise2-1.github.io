597bda20f0edfe926698d9f335cc6944a8b738e9
AnaliseWalker <analisemermaid@gmail.com>
2026-03-06 01:46:51 +0000

    finished main but still need some work on extra

--- asd-projects/dice-app/dice.js
+++ asd-projects/dice-app/dice.js
@@ -1,60 +1,59 @@
 $(document).ready(function () {
-  
-  function makeDot(top, left, elementID, color){
+  function makeDot(top, left, elementID, color) {
     $("<div>")
-    .css("height", 15)
-    .css("width", 15)
-    .css("background-color", color)
-    .css("position", "absolute")
-    .css("top", top)
-    .css("left", left)
-    .css("border-radius", "20%")
-    .appendTo(elementID);
+      .css("height", 15)
+      .css("width", 15)
+      .css("background-color", color)
+      .css("position", "absolute")
+      .css("top", top)
+      .css("left", left)
+      .css("border-radius", "20%")
+      .appendTo(elementID);
   }
 
-  function rollDie(dieID, color){
+
+  function rollDie(dieID, color) {
     $(dieID).empty();
+    var rightWidth = ($(dieID).width()-15);
+    var rightHeight = ($(dieID).height()-15);
     var randomNum = Math.ceil(Math.random() * 6);
     if (randomNum === 1) {
-      makeDot(42, 43, dieID, color); // middle middle
+      makeDot(rightWidth/2, rightHeight/2, dieID, color); // middle middle 42 43
     } else if (randomNum === 2) {
-      makeDot(20, 20, dieID, color); // top left
-      makeDot(70, 70, dieID, color); // bottom right
+      makeDot(rightWidth/4, rightHeight/4, dieID, color); // top left
+      makeDot(rightWidth/1.25, rightHeight/1.25, dieID, color); // bottom right
     } else if (randomNum === 3) {
-      makeDot(20, 20, dieID, color); // top left
-      makeDot(70, 70, dieID, color); // bottom right
-      makeDot(42, 44, dieID, color); // middle middle
+      makeDot(rightWidth/4, rightHeight/4, dieID, color); // top left
+      makeDot(rightWidth/1.25, rightHeight/1.25, dieID, color); // bottom right
+      makeDot(rightWidth/2, rightHeight/2, dieID, color); // middle middle
     } else if (randomNum === 4) {
-      makeDot(70, 70, dieID, color); // bottom right
-      makeDot(20, 20, dieID, color); // top left
-      makeDot(20, 70, dieID, color); // bottom left
-      makeDot(70, 20, dieID, color); // top right
+      makeDot(rightWidth/1.25, rightHeight/1.25, dieID, color); // bottom right
+      makeDot(rightWidth/4, rightHeight/4, dieID, color); // top left
+      makeDot(rightWidth/4, rightHeight/1.25, dieID, color); // bottom left
+      makeDot(rightWidth/1.25, rightHeight/4, dieID, color); // top right
     } else if (randomNum === 5) {
-      makeDot(42, 44, dieID, color); // middle middle
-      makeDot(70, 70, dieID, color); // bottom right
-      makeDot(20, 20, dieID, color); // top left
-      makeDot(20, 70, dieID, color); // bottom left
-      makeDot(70, 20, dieID, color); // top right
+      makeDot(rightWidth/2, rightHeight/2, dieID, color); // middle middle
+      makeDot(rightWidth/1.25, rightHeight/1.25, dieID, color); // bottom right
+      makeDot(rightWidth/4, rightHeight/4, dieID, color); // top left
+      makeDot(rightWidth/4, rightHeight/1.25, dieID, color); // bottom left
+      makeDot(rightWidth/1.25, rightHeight/4, dieID, color); // top right
     } else if (randomNum === 6) {
-      makeDot(20, 70, dieID, color);
-      makeDot(45, 70, dieID, color);
-      makeDot(70, 70, dieID, color);
-      makeDot(20, 20, dieID, color);
-      makeDot(45, 20, dieID, color);
-      makeDot(70, 20, dieID, color);
+      makeDot(rightWidth/4, rightHeight/1.25, dieID, color);
+      makeDot(45, rightHeight/1.25, dieID, color);
+      makeDot(rightWidth/1.25, rightHeight/1.25, dieID, color);
+      makeDot(rightWidth/4, rightHeight/4, dieID, color);
+      makeDot(45, rightHeight/4, dieID, color);
+      makeDot(rightWidth/1.25, rightHeight/4, dieID, color);
     }
   }
 
-  function handleClick1(){
+  function handleClick1() {
     rollDie("#die", "white");
   }
-  function handleClick2(){
+  function handleClick2() {
     rollDie("#die2", "black");
   }
 
   $("#die").on("click", handleClick1);
   // $("#die2").on("click", handleClick2);
-
 });
-
-
--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -6,6 +6,8 @@ $(document).ready(function () {
   $("#reset").on("click", resetAndRender);
 });
 
+// var pixelArray = [];
+
 /////////////////////////////////////////////////////////
 //////// event handler functions are below here /////////
 /////////////////////////////////////////////////////////
@@ -18,8 +20,32 @@ function resetAndRender() {
 
 // this function applies the filters to the image and is where you should call
 // all of your apply functions
-function applyAndRender() {
+function applyAndRender(x) {
   // Multiple TODOs: Call your apply function(s) here
+  if (x === 1){
+    $("#applyReddify").on("click", applyFilter(reddify));
+  }else if (x === 2){
+    $("#applyDecreaseBlue").on("click", applyFilter(decreaseBlue));
+  }else if (x === 3){
+    $("#applyIncreaseGreenByBlue").on("click", applyFilter(increaseGreenByBlue));
+  }else if (x === 4){
+    $("#applyPurpleTint").on("click", applyFilter(purpleTint));
+  }else if (x === 5){
+    
+  }else if (x === 6){
+    
+  }else if (x === 7){
+    
+  }else if (x === 8){
+    
+  }else if (x === 9){
+    
+  }
+  $("#applyVintageFilter").on("click", applyFilter(vintageFilter));
+  $("#applyGrayscaleFilter").on("click", applyFilter(grayscaleFilter));
+  $("#applyInvertFilter").on("click", applyFilter(invertFilter));
+  $("#applySmudgeFilter").on("click", applyFilter(smudgeFilter));
+  $("#applyBlurFilter").on("click", applyFilter(blurFilter));
 
   
 
@@ -32,18 +58,88 @@ function applyAndRender() {
 /////////////////////////////////////////////////////////
 
 // TODO 1, 2, 3 & 5: Create the applyFilter function here
+function applyFilter(filterFunction) {
+  for (let i = 0; i < image.length; i++){
+    for (let j = 0; j < image[i].length; j++){
+      // console.log(image[i][j]);
+      var pixel = image[i][j];
+      var pixelArray = rgbStringToArray(pixel);
+      // this is where ill modify the color values later
+      filterFunction(pixelArray);
+      var updatedPixel = rgbArrayToString(pixelArray);
+      image[i][j] = updatedPixel;
+    }
+  }
 
+}
 
 // TODO 9 Create the applyFilterNoBackground function
+function applyFilterNoBackground(filterFunction){
+  var backgroundColor = image[0][0];
+  for (let i = 0; i < image.length; i++){
+    for (let j = 0; j < image[i].length; j++){
+      if (image[i][j] !== backgroundColor){
+        var pixelArray = rgbStringToArray(image[i][j]);
+        filterFunction(pixelArray);
+        var updatedPixel = rgbArrayToString(pixelArray);
+        image[i][j] = updatedPixel;
 
+      }
+    }
+  }
+}
 
 // TODO 6: Create the keepInBounds function
-
+function keepInBounds(number){
+  return number < 0 ? 0 : number > 255 ? 255 : number;
+}
 
 // TODO 4: Create reddify filter function
-
+function reddify(aPixelArray){
+  if (aPixelArray[RED] < 200){
+    var amount = 200 - aPixelArray[RED];
+    aPixelArray[RED] += amount;
+  }
+}
 
 // TODO 7 & 8: Create more filter functions
+function decreaseBlue(aPixelArray){
+  aPixelArray[BLUE] -= 50;
+  keepInBounds(aPixelArray[BLUE]);
+}
 
+function increaseGreenByBlue(aPixelArray){
+  let bluePixel = aPixelArray[BLUE];
+  let greenPixel = aPixelArray[GREEN]; 
+  greenPixel += bluePixel;
+  keepInBounds(greenPixel);
+  aPixelArray[GREEN] = greenPixel;
+}
 
 // CHALLENGE code goes below here
+function purpleTint(aPixelArray){
+  aPixelArray[RED] += 100;
+  keepInBounds(aPixelArray[RED]);
+  aPixelArray[BLUE] += 100;
+  keepInBounds(aPixelArray[BLUE]);
+}
+
+function vintageFilter(){
+
+}
+
+function grayscaleFilter(){
+
+}
+
+function invertFilter(){
+
+}
+
+function smudgeFilter(){
+
+}
+
+function blurFilter(){
+
+}
